1566f43a82e7ac0d655fabd396ea827056c69ffa
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-03-22 15:44:54 -0300

    object

---
+++ Data types/Object/scripts.js
@@ -0,0 +1,19 @@
+/*
+    Object
+        properties // attributes
+        funcionalities // methods
+    
+    {property: value}
+*/
+
+console.log({
+    //empty object
+})
+
+console.log({
+    name: "Marcos",
+    age: 17,
+    walk: function() {
+        console.log("walk")
+    }
+})
