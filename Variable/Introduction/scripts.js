e6c2bd1e78162f6cf3ad77ddafabcb8bda1ef1df
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-03-22 20:38:25 -0300

    variable introduction js

---
+++ Variable/Introduction/scripts.js
@@ -0,0 +1,17 @@
+/*
+    Var -> allow to change value
+    Let -> similar to var
+    Const -> constant value
+*/
+
+var n = 2
+n = "pogChamp"
+console.log("n = " + n) //console: n = pogChamp
+
+let k = true
+k = "pog"
+console.log("k = " + k) //console: k = pog
+
+const x = 5
+x = 4 //error
+console.log("x = " + x)
