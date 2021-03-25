b8e5ffa897d605fe9da76ce5832f384eee9997e7
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-03-24 22:40:18 -0300

    updating array

--- Data types/Array/scripts.js
+++ Data types/Array/scripts.js
@@ -13,3 +13,16 @@ console.log([
     "butter",
     "wheat"
 ])
+
+const animals = [
+    'monke',
+    'bird',
+    'dog',
+    {
+        nome: 'cat',
+        age: 4
+    }
+]
+
+console.log(animals[0]+animals.[1]+animals[2]+animals[3]+animals[3].age+animals[4])
+console.log(animals.length)
