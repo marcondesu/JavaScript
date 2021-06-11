892778a5a76c3dabd7010c92666330ecde137ed7
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-10 22:51:13 -0300

    functions2
    updating hello functions and add arguments and parameters

---
+++ Functions/Arguments and parameters/scripts.js
@@ -0,0 +1,7 @@
+//function expression
+//function(parameters)
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(5, 2) //arguments
---
+++ Functions/Hello functions/scripts.js
@@ -0,0 +1,15 @@
+//function statement
+function createPhrases() {
+    console.log("Um prato de trigo para tigres tristes")
+    console.log("Quem com ferro fere com ferro será ferido")
+    console.log("Água mole pedra dura tanto bate até que fura")
+}
+
+//execute
+createPhrases()
+
+//again
+createPhrases()
+
+//and again
+createPhrases()
