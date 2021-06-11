030569d0f9a71caa0b4fe6c28d2ecb9828f537d4
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-10 23:11:21 -0300

    Add files via upload

---
+++ Functions/Returning values/scripts.js
@@ -0,0 +1,15 @@
+//function expression
+//function(parameters)
+const sum = function(number1, number2) {
+    let total = number1 + number2
+    
+    return total
+}
+
+let n1 = 2
+let n2 = 5
+
+console.log(`the number 1 is ${n1}`)
+console.log(`the number 2 is ${n2}`)
+console.log(`the sum is ${sum(n1, n2)}`)
+console.log(total)
