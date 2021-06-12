5d46919db03ae03a47889542f1c1325d7a109221
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-12 15:36:49 -0300

    function hoisting

---
+++ Functions/Function hoisting/scripts.js
@@ -0,0 +1,14 @@
+//function hoisting
+sayAmogus()
+
+function sayAmogus() {
+    console.log('Amogus')
+}
+
+/* const sayAmogus = function sayAmogus() {
+    console.log('Amogus')
+}
+
+var sayAmogus = function sayAmogus() {
+    console.log('Amogus')
+} */
