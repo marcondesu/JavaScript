395c33d3cbd14511f5d8e6404ab5d414845f4e3d
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-12 15:42:25 -0300

    arrow function

---
+++ Functions/Arrow function/scripts.js
@@ -0,0 +1,13 @@
+//arrow function
+const saySomething = () => {
+    console.log('Something')
+}
+
+saySomething()
+
+/* or
+const saySomething = (something) => {
+    console.log(something)
+}
+
+saySomething('Something) */
