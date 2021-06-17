7f72ab28cd20f9ff1a8d3f7aa4ede7d252e7058f
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-17 02:06:37 -0300

    type conversion coersion

---
+++ Manipulating data/Type conversion coersion/scripts.js
@@ -0,0 +1,8 @@
+/*
+    Type conversion (typecasting) vs. Type coersion
+
+    converts an data type into other data type
+*/
+
+console.log('3' + 5) //5 will be considered as '5' and shows 35 on the console
+console.log(Number('3') + 5) //the function Number() converts '3' into 3 and will show 8 on the console
