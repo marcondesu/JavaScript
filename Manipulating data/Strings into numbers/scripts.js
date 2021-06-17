1c6994d82757dcd5278280d1f7804b197d9ba52e
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-17 02:14:16 -0300

    strings into numbers

---
+++ Manipulating data/Strings into numbers/scripts.js
@@ -0,0 +1,11 @@
+/*
+    manipulating strings and numbers
+
+    transform strings into numbers and numbers into strings
+*/
+
+let string = "241"
+console.log(Number(string)) //will transform the value of 'string' to a Number
+
+let number = 142
+console.log(String(number)) //will transform the value of 'number' to a String
