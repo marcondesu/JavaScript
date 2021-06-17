ffe7b5388d8c51732ee99a6fee581b44654f8f17
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2021-06-17 02:22:05 -0300

    counting characters and digits

---
+++ Manipulating data/Counting characters and digits/scripts.js
@@ -0,0 +1,9 @@
+//  counting characters and digits
+
+let anything = "zimbabwe"
+let anything2_lol = "czech republic"
+let anumber = 134535
+
+console.log(anything.length)
+console.log(anything2_lol.length)
+console.log(String(anumber).length)
